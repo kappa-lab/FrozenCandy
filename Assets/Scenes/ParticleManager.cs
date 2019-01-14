ae80c6b83f9a3f517e330bf611a24c8b234c5982
yamagami kenichi <kenichi-yamagami@altplus.co.jp>
2019-01-14 23:45:48 +0900

    random

---
+++ Assets/Scenes/ParticleManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class ParticleManager : MonoBehaviour {
+
+    public Spawn prefab;
+    public List<Spawn> spawns= new List<Spawn>();
+    public List<Marker> initialMarkers = new List<Marker>();
+
+
+    public void Create(Marker from, Marker to)
+    {
+        var s = Instantiate<Spawn>(prefab,transform);
+        s.onArrived += OnArrived;
+        s.RegisterMarker(from, to );
+        spawns.Add(s);
+    }
+
+    private void Start()
+    {
+        foreach (var m in initialMarkers)
+        {
+            foreach (var n in m.next)
+            {
+                Create(m, n);
+            }
+        }
+    }
+
+    private void OnArrived(Spawn sp, Marker marker) {
+        var shuffle = sp.to.next.OrderBy(i => Guid.NewGuid()).ToList();
+
+        if (marker.next.Length==1)
+        {
+            sp.RegisterMarker(marker, marker.next[0]);
+            return;
+        }
+        else if (marker.next.Length == 0)
+        {
+            var initm = initialMarkers[0];
+            shuffle = initm.next.OrderBy(i => Guid.NewGuid()).ToList();
+            sp.RegisterMarker(initm, shuffle[0]);
+            return;
+        }
+
+
+        sp.RegisterMarker(sp.to, shuffle[0]);
+        shuffle.RemoveAt(0);
+        foreach (var m in shuffle)
+        {
+            Debug.Log(m.transform.position);
+            if (spawns.Count < 12) {
+                Create(marker, m);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scenes/Spawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class Spawn : MonoBehaviour {
+    public Marker from;
+    public Marker to;
+
+    public delegate void OnArrived(Spawn spawn, Marker marker);
+
+    public event OnArrived onArrived;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RegisterMarker(Marker from, Marker to) { 
+        this.from = from;
+        this.to = to;
+
+        var d = Vector3.Distance(from.transform.position, to.transform.position);
+
+        transform.position = from.transform.position;
+        transform.DOMove(to.transform.position, d * 0.7f)
+            .SetEase(Ease.Linear)
+            .OnComplete(Next);
+    }
+
+    private void Next()
+    {
+        if(onArrived != null) onArrived.Invoke(this, to);
+    }
+}
