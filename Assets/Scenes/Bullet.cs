037ff5343bbf3e02a9e17bce173a85ca7b718bf3
yamagami kenichi <gamiken@kappa-lab.com>
2020-01-26 02:29:23 +0900

    Explosion

--- Assets/Scenes/Bullet.cs
+++ Assets/Scenes/Bullet.cs
@@ -8,30 +8,44 @@ public class Bullet : MonoBehaviour
     public float lifeTime = 1.0f;
     public Terrain wall;
     public long Id;
+    public ParticleSystem explosion;
+    public ParticleSystem warhead;
+    private Tween tween;
     void Start()
     {
         var tgt = 0f;
         lifeTime = Random.Range(4f, 6.6f);
-        DOTween
+        tween = DOTween
             .To(() => life, (x) => life = x, tgt, lifeTime)
             .OnComplete(SelfDelete);
 
-
+        explosion.gameObject.SetActive(false);
     }
 
     void SelfDelete() {
         Destroy(gameObject);
     }
     // Update is called once per frame
-    void Update()
+    void Explode()
     {
+        explosion.transform.SetParent(transform.root, true);
+        explosion.gameObject.SetActive(true);
 
+        tween.Kill();
+
+        var l = 1f;
+        tween = DOTween
+            .To(() => l, (x) => l = x, 0, explosion.duration)
+            .OnComplete(()=> { Destroy(explosion.gameObject); });
+
+        SelfDelete();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.gameObject == wall.gameObject) {
             Debug.Log(Id + ": " +collision.collider.gameObject);
+            Explode();
         }
     }
 }
