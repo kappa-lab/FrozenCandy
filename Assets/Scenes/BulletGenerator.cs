be21d5b3bf0b4b91915f248be88cf931522d2558
yamagami kenichi <gamiken@kappa-lab.com>
2020-01-30 03:27:26 +0900

    refactor

--- Assets/Scenes/BulletGenerator.cs
+++ Assets/Scenes/BulletGenerator.cs
@@ -12,18 +12,22 @@ public class BulletGenerator : MonoBehaviour
             this.max = max;
         }
     }
+
     public Bullet bullet;
     public bool iginitionOnCreate = false;
-
-    public float charge = 0;
     public Terrain wall;
-    public float creationStep = 0.0f;
-    public float numCreation;
+    public float creationStep = 0.4f;
+
     public Range createionRange = new Range(20, 20);
-    public long numBullet = 0;
     public Range xRange = new Range(-9, 9);
     public Range yRange = new Range(4, 8);
     public Range zRange = new Range(-3, 3);
+
+    [SerializeField, Space(15), Header("== readonly ==")
+    ]private float _charge = 0;
+    [SerializeField] private float _creationPhase = 0f;
+    [SerializeField] private float _numCreation = 0;
+    [SerializeField] private long _numBullet = 0;
 
     void OnEnable()    
     {
@@ -35,23 +39,23 @@ public class BulletGenerator : MonoBehaviour
         if (!enabled) return;
         var chargeTime = Random.Range(.16f, .24f);
         DOTween
-            .To(() => charge, (x) => charge = x, 1f, chargeTime)
+            .To(() => _charge, (x) => _charge = x, 1f, chargeTime)
             .OnComplete(OnCharged);
     }
 
     void OnCharged() {
         Generate();
-        charge = 0;
+        _charge = 0;
         StartCharge();
     }
 
     void Generate()
     {
         var q = new Quaternion();
-        creationStep += 0.04f;
-        var cof = (Mathf.Sin(creationStep) + 1) / 2;
-        numCreation = (createionRange.max - createionRange.min) * cof + createionRange.min;
-        for (int i = 0; i < numCreation; i++)
+        _creationPhase += creationStep;
+        var cof = (Mathf.Sin(_creationPhase) + 1) / 2;
+        _numCreation = (createionRange.max - createionRange.min) * cof + createionRange.min;
+        for (int i = 0; i < _numCreation; i++)
         {
             var p = new Vector3(
                 Random.Range(xRange.min, xRange.max),
@@ -62,7 +66,7 @@ public class BulletGenerator : MonoBehaviour
             var b = Instantiate(bullet, this.transform,false) as Bullet;
             b.transform.localPosition = p;
             b.wall = wall;
-            b.Id = numBullet++;
+            b.Id = _numBullet++;
 
             if (iginitionOnCreate) b.Ignition();
             
